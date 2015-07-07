1c62f9df8eea9ac957b51911d3c952f1f452ad8f
Lenworth Rose <lenworthrose@gmail.com>
2015-07-07 19:54:13 -0400

    Widget's play/pause button updates based on state

--- app/src/main/java/com/lenworthrose/music/widget/WidgetService.java
+++ app/src/main/java/com/lenworthrose/music/widget/WidgetService.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.IBinder;
 import android.support.v4.content.LocalBroadcastManager;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.RemoteViews;
 
@@ -16,6 +17,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.request.target.AppWidgetTarget;
 import com.lenworthrose.music.R;
 import com.lenworthrose.music.util.Constants;
+import com.lenworthrose.music.util.Constants.PlaybackState;
 
 /**
  * Service responsible for updating Widgets.
@@ -39,22 +41,47 @@ public class WidgetService extends Service {
             public void onReceive(Context context, Intent intent) {
                 final AppWidgetManager appMan = AppWidgetManager.getInstance(context);
                 final int[] appWidgetIds = appMan.getAppWidgetIds(new ComponentName(context, WidgetProvider.class));
-                String artist = intent.getStringExtra(Constants.EXTRA_ARTIST);
 
-                for (int appWidgetId : appWidgetIds) {
-                    RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_view);
-                    rv.setTextViewText(R.id.widget_title, intent.getStringExtra(Constants.EXTRA_TITLE));
-                    rv.setTextViewText(R.id.widget_subtitle, artist);
-                    rv.setViewVisibility(R.id.widget_subtitle, artist == null || artist.isEmpty() ? View.GONE : View.VISIBLE);
-                    appMan.partiallyUpdateAppWidget(appWidgetId, rv);
+                switch (intent.getAction()) {
+                    case Constants.PLAYING_NOW_CHANGED:
+                        String artist = intent.getStringExtra(Constants.EXTRA_ARTIST);
 
-                    Glide.with(context).load(intent.getStringExtra(Constants.EXTRA_ALBUM_ART_URL)).asBitmap().fallback(R.drawable.logo)
-                            .error(R.drawable.logo).into(new AppWidgetTarget(WidgetService.this, rv, R.id.widget_image, appWidgetIds));
+                        for (int appWidgetId : appWidgetIds) {
+                            RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_view);
+                            rv.setTextViewText(R.id.widget_title, intent.getStringExtra(Constants.EXTRA_TITLE));
+                            rv.setTextViewText(R.id.widget_subtitle, artist);
+                            rv.setViewVisibility(R.id.widget_subtitle, TextUtils.isEmpty(artist) ? View.GONE : View.VISIBLE);
+                            appMan.partiallyUpdateAppWidget(appWidgetId, rv);
+
+                            Glide.with(context).load(intent.getStringExtra(Constants.EXTRA_ALBUM_ART_URL)).asBitmap().fallback(R.drawable.logo)
+                                    .error(R.drawable.logo).into(new AppWidgetTarget(WidgetService.this, rv, R.id.widget_image, appWidgetIds));
+                        }
+
+                        break;
+                    case Constants.PLAYBACK_STATE_CHANGED:
+                        PlaybackState state = (PlaybackState)intent.getSerializableExtra(Constants.EXTRA_STATE);
+                        RemoteViews rv = new RemoteViews(context.getPackageName(), R.layout.widget_view);
+
+                        switch (state) {
+                            case PLAYING:
+                                rv.setImageViewResource(R.id.widget_play_pause, R.drawable.pause);
+                                break;
+                            default:
+                                rv.setImageViewResource(R.id.widget_play_pause, R.drawable.play);
+                                break;
+                        }
+
+                        for (int appWidgetId : appWidgetIds)
+                            appMan.partiallyUpdateAppWidget(appWidgetId, rv);
+
+                        break;
                 }
             }
         };
 
-        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, new IntentFilter(Constants.PLAYING_NOW_CHANGED));
+        IntentFilter filter = new IntentFilter(Constants.PLAYING_NOW_CHANGED);
+        filter.addAction(Constants.PLAYBACK_STATE_CHANGED);
+        LocalBroadcastManager.getInstance(this).registerReceiver(receiver, filter);
     }
 
     @Override
