dba7d8f15889e049182d7e795b593173955d0be7
Lenworth Rose <lenworthrose@gmail.com>
2015-07-27 00:23:11 -0400

    CoverArtSearchTask injects art into MediaStore

--- app/src/main/java/com/lenworthrose/music/sync/CoverArtSearchTask.java
+++ app/src/main/java/com/lenworthrose/music/sync/CoverArtSearchTask.java
@@ -1,12 +1,15 @@
 package com.lenworthrose.music.sync;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
-import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.provider.MediaStore;
 import android.text.TextUtils;
 import android.util.Log;
+
+import com.lenworthrose.music.util.Utils;
 
 import java.io.File;
 import java.io.FilenameFilter;
@@ -31,23 +34,24 @@ public class CoverArtSearchTask extends AsyncTask<Context, Integer, Void> {
 
             do {
                 publishProgress(current++, total);
+                long albumId = albumsMissingArt.getLong(0);
 
                 Cursor songs = context.getContentResolver().query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
                         new String[] { MediaStore.Audio.Media.DATA },
                         MediaStore.Audio.Media.ALBUM_ID + "=?",
-                        new String[] { String.valueOf(albumsMissingArt.getLong(0)) },
+                        new String[] { String.valueOf(albumId) },
                         MediaStore.Audio.Media.TRACK);
 
                 if (!songs.moveToFirst()) continue;
 
-                Log.w("Testing", "---------------" + songs.getString(0));
                 File file = new File(songs.getString(0));
 
                 if (!file.exists()) {
-                    Log.w("CoverArtSearchTask", "Path for song in MediaStore does not exist? path=" + songs.getString(0));
+                    Log.w("CoverArtSearchTask", "Path for song in MediaStore does not exist?? path=" + songs.getString(0));
                     continue;
                 }
 
+                songs.close();
                 String parentStr = file.getParent();
                 if (TextUtils.isEmpty(parentStr)) continue;
 
@@ -57,16 +61,22 @@ public class CoverArtSearchTask extends AsyncTask<Context, Integer, Void> {
                 File[] images = parentDir.listFiles(new ImageFilenameFilter());
 
                 if (images != null && images.length > 0) {
-                    File[] allFiles = parentDir.listFiles();
-                    String[] filesToScan = new String[allFiles.length];
+                    File biggestArt = images[0];
 
-                    for (int i = 0; i < allFiles.length; i++)
-                        filesToScan[i] = allFiles[i].getAbsolutePath();
+                    for (int i = 1; i < images.length; i++)
+                        if (biggestArt.length() < images[i].length())
+                            biggestArt = images[i];
 
-                    MediaScannerConnection.scanFile(context, filesToScan, null, null);
+                    ContentValues values = new ContentValues();
+                    values.put(MediaStore.Audio.Albums.ALBUM_ID, albumId);
+                    values.put(MediaStore.Audio.Media.DATA, biggestArt.getAbsolutePath());
+
+                    context.getContentResolver().insert(Uri.parse(Utils.buildAlbumArtUrl(albumId)), values);
                 }
             } while (albumsMissingArt.moveToNext());
         }
+
+        albumsMissingArt.close();
 
         return null;
     }
